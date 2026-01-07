ced608d3b6303aac72a438f6af3653fd640a03d0
pijush512 <psagorsarker512@gmail.com>
2026-01-07 23:43:49 +0600

    problem-3 Done

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+// ? Problem 3: Check for Palindrome
+// Write a function that checks if a string is a palindrome (reads the same forward and backward).
+
+const Palindrome = (str) => {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+
+  if (str === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(Palindrome("madam"));
+console.log(Palindrome("hello"));
